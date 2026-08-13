cc2512e25563fa0e3deb58a93874d563eb644ea0
ReshmaSreeVaranasi <varanasireshmasree.25.csm@anits.edu.in>
2026-08-13 21:47:03 +0530

    Create BasicArray.java

---
+++ Practice Progs/BasicArray.java
@@ -0,0 +1,14 @@
+package javacore;
+
+public class BasicArray {
+    public static void main(String[] args) {
+
+        int x[] = {10,20,30,40,50};
+
+        for(int b:x) {
+            System.out.println(b*2);
+        }
+        int y[] = new int[5];
+
+    }
+}
